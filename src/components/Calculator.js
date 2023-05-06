a5b72faca1c804637a9ba211e4f30c4a0bbe4524
Mohammad Suliman joya <sjoya66@gmail.com>
2023-05-06 23:46:30 +0430

    Fix linter error

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,11 +1,25 @@
-import Buttons from './Buttons';
+import { useState } from 'react';
+import Button from './Buttons';
+import Calculate from '../logic/calculate';
 
-function Calculator() {
+export default function Calculator() {
+  const [show, setShow] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
   return (
-    <div>
-      <Buttons />
-    </div>
+    <main className="combined">
+      <p className="let-us">Let&apos;s do some maths!!</p>
+      <div className="display">{show.next || show.total || 0}</div>
+      <Button
+        fetcher={(e) => {
+          const valueClicked = e.target.innerHTML;
+          const result = Calculate(show, valueClicked);
+          setShow(result);
+        }}
+      />
+    </main>
   );
 }
-
-export default Calculator;
